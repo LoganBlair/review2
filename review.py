463f8fc44ae7da91a7ca9e0cdd0f5b4669f7a337
LoganBlair <s430720@student.roundrockisd.org>
2014-10-23 15:03:37 -0500

    Finished review

--- review.py
+++ review.py
@@ -11,10 +11,16 @@
 #           6
 #           8
 # Who do we appreciate?
-
+# Mr.Edegley!
 # Use a loop to complete this, either a for loop or a while loop.
 # Each time through the loop, print out either 2, 4, 6 or 8.
 # When the loop is finished, print "Who do we appreciate?"
 # Remember how range can be used to create the list [2,4,6,8]
 # When complete, commit to github and submit a pull request!
+
+for i in range(2,10,2):
+    print i
+    
+print 'Who do we appriciate?'
+
 
